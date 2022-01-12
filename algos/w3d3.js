0bfdca15807de4c9a9d3f3ab503725780acf019c
Tom Bixler <tbix45@gmail.com>
2022-01-11 16:46:10 -0800

    algos

--- algos/w3d3.js
+++ algos/w3d3.js
@@ -1,24 +1,29 @@
-//parens valid
-// return boolean if parentheses are valid (open before close and a pair) 
-function parensValid(str) {
+// //parens valid
+// // return boolean if parentheses are valid (open before close and a pair) 
+// function parensValid(str) {
+//     var open = 0;
+//     var closed = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === ")") {
+//             closed += 1;
+//             if (closed > open) {
+//                 return false
+//             }
+//         } else if (str[i] === "(") {
+//             open += 1;
+//         }
+//     }
+//     // console.log(open)
+//     // console.log(closed)
+//     return open == closed
+// }
+// console.log(parensValid("(doj))(o)"))
+
+//another all parens valid
+// return boolean
+function isAllParensValid(str) {
     var open = 0;
     var closed = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === ")") {
-            closed += 1;
-            if (closed > open) {
-                return false
-            }
-        } else if (str[i] === "(") {
-            open += 1;
-        }
-    }
-    // console.log(open)
-    // console.log(closed)
-    if (open == closed) {
-        return true
-    } else {
-        return false
-    }
+
 }
-console.log(parensValid("(doj)(o)"))
+console.log(isAllParensValid("({dojo]})"))
