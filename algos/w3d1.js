c7e0acc61b77410a6d56b02c808ab50b631615d0
Tom Bixler <tbix45@gmail.com>
2022-01-21 18:03:53 -0800

    algos

--- algos/w3d1.js
+++ algos/w3d1.js
@@ -37,3 +37,19 @@ if (myVar) {
 } else {
     console.log("myVar is null")
 }
+
+function isPrime(n) {
+    if (n < 2) {
+        return `${n} is not a prime number`
+    }
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return `${n} is not a prime number`
+        }
+    }
+    return `${n} is a prime number`
+}
+console.log(isPrime(4))
+console.log(isPrime(17))
+console.log(isPrime(133))
+console.log(isPrime(123))
